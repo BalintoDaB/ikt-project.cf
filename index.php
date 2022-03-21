51f37d0f205febef47b8de0fee3a0a940f866da3
BalintoDaB <balintm2006@gmail.com>
2022-03-21 13:43:45 +0100

    alap 2

--- index.php
+++ index.php
@@ -4,9 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>XD</h1>
+    <div class="container">
+        <div id="carouselPictures" class="carousel slide" data-bs-ride="carousel" >
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselPictures" data-bs-slide-to="0" class="active"></button>
+                    <button type="button" data-bs-target="#carouselPictures" data-bs-slide-to="1" ></button>
+                    <button type="button" data-bs-target="#carouselPictures" data-bs-slide-to="2" ></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/0.png" alt="Víztükör" class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/1.png" alt="Nádas" class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/2.png" alt="Tavirózsák" class="d-block w-100">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselPictures" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselPictures" data-bs-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </button>
+        </div>
+    </div>
+
+    <script src="main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
