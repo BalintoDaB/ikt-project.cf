6731d21145e60b21b3c543b30bcd5c88c8f097d6
BalintoDaB <balintm2006@gmail.com>
2022-03-28 14:20:15 +0200

    animations

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var intervalId = window.setInterval(function () {
   GetSlide();
-}, 50);
+}, 100);
 
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
@@ -19,7 +19,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function GetSlide() {
   var aktiv = document.getElementsByClassName("active");
-  var src = aktiv[0].getElementsByTagName("img").src;
+  var src = aktiv[1].getElementsByClassName("cr-kep");
   var hatter = document.body.style.backgroundColor;
   var color = ["#d72631", "#ff5b65", "#a2d5c6", "#077b8a", "#5c3c92"];
+  if(src[0].src.includes("0.png")){
+    document.getElementById("sec1").style.backgroundColor = color[0];
+    console.log("0");
+  }else if(src[0].src.includes("1.png")){
+    document.getElementById("sec1").style.backgroundColor = color[1];
+    console.log("1");
+  }else if(src[0].src.includes("2.png")){
+    document.getElementById("sec1").style.backgroundColor = color[2];
+    console.log("2");
+  }else if(src[0].src.includes("3.png")){
+    document.getElementById("sec1").style.backgroundColor = color[3];
+    console.log("3");
+  }else if(src[0].src.includes("4.png")){
+    document.getElementById("sec1").style.backgroundColor = color[4];
+    console.log("4");
+  }
 }
+function nagyobbit() {
+  
+}
+
