44ade3fe6791927768efad35d86d93dc0ba9e5a2
milanmarko <marko.milan@students.jedlik.eu>
2022-04-03 12:17:46 +0200

    Leegyszerűsített termek.php, nincs szükség a termekbetolt.php-ra

--- webshop/termek.php
+++ webshop/termek.php
@@ -18,6 +18,9 @@
             if($eredetiertekeles == ''){
                 $eredetiertekeles = 'N.A.';
             }
+            else{
+                $ertekeles = substr($eredetiertekeles, 0, 4);
+            }
         }
         echo "<title>$termeknev</title>"
     ?>
@@ -43,10 +46,70 @@
             </div>
         </nav>
     </header>
-    <div class="container">
-        <?php
-            include('termekbetolt.php');
-        ?>
+    <div class="container termekcont shadow-lg p-4">
+    <h1 class="text-center mb-4"><?php echo"$termeknev";?></h1>
+    <div class="row mb-4">
+        <div class="col-12 col-md-6 rounded">
+            <img class="mx-auto d-block w-25" src="../img/<?php echo$termekkep; ?>">
+        </div>
+        <div class="col-12 col-md-6 p-4 text-center">
+            <h2 class="mb-4">Termék ára: <?php echo"$termekar";?> Ft</h2>
+            <h2>Értékelés: 5/<?php echo"$ertekeles";?></h2>
+            <input type="button" class="webshopgomb btn mt-4" onclick="ugorj('webshop.php?eredetitermek=<?php echo$termeknev;?>&eredetiar=<?php echo$termekar?>')" value="Kosárba tétel">
+        </div>
+    </div>
+    <hr>
+    <div class="row mt-4">
+        <div class="col-12 col-md-6 text-center">
+            <h3>Termék értékelése</h3>
+            <form method="post">
+                <div class="row mt-4">
+                    <div class="col-12 col-md-8">
+                        <input type="range" class="ml-auto" oninput="ratekiir()" name="ratein" value="3" min="1" max="5" id="range">
+                    </div>
+                    <div class="col-12 col-md-4 text-center">
+                        <h4 id="rateszam" class="text-center mx-auto">5</h4>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-12 col-md-12">
+                        <input type="submit" class="btn webshopgomb" value="Értékelés" name="ratinglead">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-12 col-md-6 text-center">
+            <h3>Comment írás</h3>
+            <form action="post">
+                <div class="row mt-4">
+                    <div class="col-12 col-md-6">
+                        <div class="form-floating mb-3">
+                            <input type="text" name="nevin" class="form-control" id="nevin" placeholder="Molnár Bálint">
+                            <label for="nevin">Név</label>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="form-floating mb-3">
+                            <input type="text" name="kodin" class="form-control" id="kodin" placeholder="325235">
+                            <label for="kodin">Rendelést azonosító kód</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-12">
+                        <div class="mb-3">
+                            <textarea class="form-control" name="commentin" placeholder="Komment" id="commenttextarea"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-4">
+                        <input type="submit" class="btn webshopgomb" value="Közzététel" name="ratinglead">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     </div>
     <script>
         if(window.history.replaceState) 
