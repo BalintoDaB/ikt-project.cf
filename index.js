ea110ce3fadd32b2c3651bf50e4ac046f6e4cead
BalintoDaB <balintm2006@gmail.com>
2022-05-05 22:49:09 +0200

    anim test

---
+++ index.js
@@ -0,0 +1,12 @@
+function RanInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     anime({
+//         targets: '.betolt',
+//         translateX: 1,
+//         translateY: 1,
+//         easing: 'spring(1, 80, 10, 0)'
+//     });
+// });
